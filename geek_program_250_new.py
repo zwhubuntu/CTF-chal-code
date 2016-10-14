eda56e7bb462065aab8c02a320882a4c443609df
zwhubuntu <zwhubuntu@hotmail.com>
2016-10-14 12:12:08 +0800

    Initial commit
    new file:   0CTF_monkey.py
    	new file:   45.bmp
    	new file:   CCTF_rev100_2.py
    	new file:   DCTF_RE100.py
    	new file:   PCTF_BABY_crackme.py
    	new file:   PCTF_BACON_DECRPT.py
    	new file:   PCTF_FINDKEY_CRACKME.py
    	new file:   PCTF_GCD_TEST.py
    	new file:   PCTF_MISC150.py
    	new file:   PCTF_MISC_250.py
    	new file:   PCTF_VERY_HARDRSA.py
    	new file:   PCTF_WEB100.py
    	new file:   PCTF_hard_rsa.py
    	new file:   PCTF_shellcode.py
    	new file:   PCTF_smali_reverse.py
    	new file:   SSCTF_C100_TEST.py
    	new file:   asciidic.py
    	new file:   backdoor_checksum.py
    	new file:   backdoor_ctf_web500.py
    	new file:   backdoor_prime_sum_misc75.py
    	new file:   bacon.py
    	new file:   bamboofox_genlist.py
    	new file:   bamboofox_kobesystem.py
    	new file:   bamboofox_loginsystem.py
    	new file:   bamboofox_sqli.py
    	new file:   baopo.py
    	new file:   baopo.pyc
    	new file:   base64_bianzhong.py
    	new file:   bin_2_hexstream.py
    	new file:   bin_2_hexstream.pyc
    	new file:   bmp_check.py
    	new file:   brainfuck.py
    	new file:   cctf_re2_test.py
    	new file:   chk_flag.py
    	new file:   chk_flag.pyc
    	new file:   chk_image
    	new file:   chk_img_files.py
    	new file:   chk_img_files.pyc
    	new file:   cmcc_mongodb_inject.py
    	new file:   cmcc_program1.py
    	new file:   cmcc_program2.py
    	new file:   cmcc_program3.py
    	new file:   cmcc_program4.py
    	new file:   cmcc_program5.py
    	new file:   cmcc_program8.py
    	new file:   cmcc_program9.py
    	new file:   cmcc_programe7.py
    	new file:   cmcc_py_rev.py
    	new file:   cmcc_xmlinject.py
    	new file:   crackme_2_py.py
    	new file:   csco_decrpt.py
    	new file:   csco_decrpt.pyc
    	new file:   day_dic_set.py
    	new file:   day_dic_set.pyc
    	new file:   decode.py
    	new file:   decode.pyc
    	new file:   dic_ocr.py
    	new file:   dic_ocr.pyc
    	new file:   fence_ciper.py
    	new file:   gctf_misc100.py
    	new file:   gen_primes.py
    	new file:   hack_rsa.py
    	new file:   hack_rsa.pyc
    	new file:   hctf_mobile100.py
    	new file:   hk_rsa.py
    	new file:   ichunqiu_base64.py
    	new file:   ichunqiu_crypto_100.py
    	new file:   idf_py_rev.py
    	new file:   image_check.py
    	new file:   image_check.pyc
    	new file:   inner_netpatch.py
    	new file:   interwatchCTF_CODE50.py

--- geek_program_250_new.py
+++ geek_program_250_new.py
@@ -8,19 +8,22 @@ for i in range(0, 300)[::-1]:
     #put_path =path +str(i + 1)
     file = open(extract_path, 'rb')
     strr = file.read()
-    if "\x50\x4B\x03\x04" in strr:
-        tar = ZipFile(extract_path)
-        tar.extract(str(i - 1), path)
-        tar.close()
+    try:
+        if "\x50\x4B\x03\x04" in strr:
+            tar = ZipFile(extract_path)
+            tar.extract(str(i - 1), path)
+            tar.close()
+            file.close()
+        elif "\x1F\x8B\x08" in strr:
+            tar = TarFile.gzopen(extract_path)
+            tar.extract(str(i - 1), path)
+            tar.close()
+            file.close()
+        elif "\x42\x5A\x68\x39" in strr:
+            tar = TarFile.bz2open(extract_path)
+            tar.extract(str(i - 1), path)
+            tar.close()
+            file.close()
         file.close()
-    elif "\x1F\x8B\x08" in strr:
-        tar = TarFile.gzopen(extract_path)
-        tar.extract(str(i - 1), path)
-        tar.close()
-        file.close()
-    elif "\x42\x5A\x68\x39" in strr:
-        tar = TarFile.bz2open(extract_path)
-        tar.extract(str(i - 1), path)
-        tar.close()
-        file.close()
-    file.close()
+    except:
+        print "something error!"
