bd51287e55d91ea40fbaf2f70e10eeb3cb464bc5
zwhubuntu <zwhubuntu@hotmail.com>
2016-10-15 23:19:56 +0800

    Initial commit
    new file:   0CTF_monkey.py
    	new file:   45.bmp
    	new file:   CCTF_rev100_2.py
    	new file:   DCTF_RE100.py
    	new file:   PCTF_BABY_crackme.py
    	new file:   PCTF_BACON_DECRPT.py
    	new file:   PCTF_FINDKEY_CRACKME.py
    	new file:   PCTF_GCD_TEST.py
    	new file:   PCTF_MISC150.py
    	new file:   PCTF_MISC_250.py
    	new file:   PCTF_VERY_HARDRSA.py
    	new file:   PCTF_WEB100.py
    	new file:   PCTF_hard_rsa.py
    	new file:   PCTF_shellcode.py
    	new file:   PCTF_smali_reverse.py
    	new file:   SSCTF_C100_TEST.py
    	new file:   asciidic.py
    	new file:   backdoor_checksum.py
    	new file:   backdoor_ctf_web500.py
    	new file:   backdoor_prime_sum_misc75.py
    	new file:   bacon.py
    	new file:   bamboofox_genlist.py
    	new file:   bamboofox_kobesystem.py
    	new file:   bamboofox_loginsystem.py
    	new file:   bamboofox_sqli.py
    	new file:   baopo.py
    	new file:   baopo.pyc
    	new file:   base64_bianzhong.py
    	new file:   bin_2_hexstream.py
    	new file:   bin_2_hexstream.pyc
    	new file:   bmp_check.py
    	new file:   brainfuck.py
    	new file:   cctf_re2_test.py
    	new file:   chk_flag.py
    	new file:   chk_flag.pyc
    	new file:   chk_image
    	new file:   chk_img_files.py
    	new file:   chk_img_files.pyc
    	new file:   cmcc_mongodb_inject.py
    	new file:   cmcc_program1.py
    	new file:   cmcc_program2.py
    	new file:   cmcc_program3.py
    	new file:   cmcc_program4.py
    	new file:   cmcc_program5.py
    	new file:   cmcc_program8.py
    	new file:   cmcc_program9.py
    	new file:   cmcc_programe7.py
    	new file:   cmcc_py_rev.py
    	new file:   cmcc_xmlinject.py
    	new file:   crackme_2_py.py
    	new file:   csco_decrpt.py
    	new file:   csco_decrpt.pyc
    	new file:   day_dic_set.py
    	new file:   day_dic_set.pyc
    	new file:   decode.py
    	new file:   decode.pyc
    	new file:   dic_ocr.py
    	new file:   dic_ocr.pyc
    	new file:   fence_ciper.py
    	new file:   gctf_misc100.py
    	new file:   gen_primes.py
    	new file:   hack_rsa.py
    	new file:   hack_rsa.pyc
    	new file:   hctf_mobile100.py
    	new file:   hk_rsa.py
    	new file:   ichunqiu_base64.py
    	new file:   ichunqiu_crypto_100.py
    	new file:   idf_py_rev.py
    	new file:   image_check.py
    	new file:   image_check.pyc
    	new file:   inner_netpatch.py
    	new file:   interwatchCTF_CODE50.py

---
+++ simplexue_easykeygen.py
@@ -0,0 +1,61 @@
+'''
+username = 0;
+  v15 = 0;
+  memset(&v12, 0, 0x60u);
+  v13 = 0;
+  v14 = 0;
+  memset(&v16, 0, 0xC4u);
+  v17 = 0;
+  v18 = 0;
+  v8 = 16;
+  v9 = 32;
+  v10 = 48;
+  sub_4011B9((int)aInputName, v6);
+  scanf(aS, &username);
+  v3 = 0;
+  for ( i = 0; v3 < (signed int)strlen(&username); ++i )
+  {
+    if ( i >= 3 )
+      i = 0;
+    sprintf(&v15, aS02x, &v15, *(&username + v3++) ^ *(&v8 + i));
+  }
+  memset(&username, 0, 0x64u);
+  sub_4011B9((int)aInputSerial, v7);
+  scanf(aS, &username);
+  if ( !strcmp(&username, &v15) )
+  {
+    sub_4011B9((int)aCorrect, *(int *)&v8);
+    result = 0;
+  }
+  else
+  {
+    sub_4011B9((int)aWrong, *(int *)&v8);
+    result = 0;
+  }
+  return result;
+
+'''
+lst = [16, 32, 48]
+username = 'FlappyPig'
+crack_username = ''
+serial = ''
+for i in xrange(len(username)):
+    if i % 3 == 0:
+        serial += chr(ord(username[i]) ^ lst[0]).encode('hex')
+    elif i % 3 == 1:
+        serial += chr(ord(username[i]) ^ lst[1]).encode('hex')
+    elif i % 3 == 2:
+        serial += chr(ord(username[i]) ^ lst[2]).encode('hex')
+
+print serial
+
+crack_serial = [0x5D, 0x4F, 0x7A, 0x25, 0x52, 0x46, 0x21, 0x53]
+for i in xrange(len(crack_serial)):
+    if i % 3 == 0:
+        crack_username += chr(crack_serial[i] ^ lst[0])
+    elif i % 3 == 1:
+        crack_username += chr(crack_serial[i] ^ lst[1])
+    elif i % 3 == 2:
+        crack_username += chr(crack_serial[i] ^ lst[2])
+
+print crack_username
