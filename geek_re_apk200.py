a3553c3c068a6f096d7203b6081793abe03e2768
zwhubuntu <zwhubuntu@hotmail.com>
2016-10-17 12:35:36 +0800

    Initial commit
    new file:   0CTF_monkey.py
    	new file:   45.bmp
    	new file:   CCTF_rev100_2.py
    	new file:   DCTF_RE100.py
    	new file:   PCTF_BABY_crackme.py
    	new file:   PCTF_BACON_DECRPT.py
    	new file:   PCTF_FINDKEY_CRACKME.py
    	new file:   PCTF_GCD_TEST.py
    	new file:   PCTF_MISC150.py
    	new file:   PCTF_MISC_250.py
    	new file:   PCTF_VERY_HARDRSA.py
    	new file:   PCTF_WEB100.py
    	new file:   PCTF_hard_rsa.py
    	new file:   PCTF_shellcode.py
    	new file:   PCTF_smali_reverse.py
    	new file:   SSCTF_C100_TEST.py
    	new file:   asciidic.py
    	new file:   backdoor_checksum.py
    	new file:   backdoor_ctf_web500.py
    	new file:   backdoor_prime_sum_misc75.py
    	new file:   bacon.py
    	new file:   bamboofox_genlist.py
    	new file:   bamboofox_kobesystem.py
    	new file:   bamboofox_loginsystem.py
    	new file:   bamboofox_sqli.py
    	new file:   baopo.py
    	new file:   baopo.pyc
    	new file:   base64_bianzhong.py
    	new file:   bin_2_hexstream.py
    	new file:   bin_2_hexstream.pyc
    	new file:   bmp_check.py
    	new file:   brainfuck.py
    	new file:   cctf_re2_test.py
    	new file:   chk_flag.py
    	new file:   chk_flag.pyc
    	new file:   chk_image
    	new file:   chk_img_files.py
    	new file:   chk_img_files.pyc
    	new file:   cmcc_mongodb_inject.py
    	new file:   cmcc_program1.py
    	new file:   cmcc_program2.py
    	new file:   cmcc_program3.py
    	new file:   cmcc_program4.py
    	new file:   cmcc_program5.py
    	new file:   cmcc_program8.py
    	new file:   cmcc_program9.py
    	new file:   cmcc_programe7.py
    	new file:   cmcc_py_rev.py
    	new file:   cmcc_xmlinject.py
    	new file:   crackme_2_py.py
    	new file:   csco_decrpt.py
    	new file:   csco_decrpt.pyc
    	new file:   day_dic_set.py
    	new file:   day_dic_set.pyc
    	new file:   decode.py
    	new file:   decode.pyc
    	new file:   dic_ocr.py
    	new file:   dic_ocr.pyc
    	new file:   fence_ciper.py
    	new file:   gctf_misc100.py
    	new file:   gen_primes.py
    	new file:   hack_rsa.py
    	new file:   hack_rsa.pyc
    	new file:   hctf_mobile100.py
    	new file:   hk_rsa.py
    	new file:   ichunqiu_base64.py
    	new file:   ichunqiu_crypto_100.py
    	new file:   idf_py_rev.py
    	new file:   image_check.py
    	new file:   image_check.pyc
    	new file:   inner_netpatch.py
    	new file:   interwatchCTF_CODE50.py

---
+++ geek_re_apk200.py
@@ -0,0 +1,152 @@
+'''
+public void appInit() {
+        this.btnCheck = this.findViewById(2131492945);
+        this.usrName = this.findViewById(2131492950);
+        this.usrPass = this.findViewById(2131492949);
+        this.keyShow = this.findViewById(2131492946);
+        this.keyShow.setText("Have FUN With GeekChal2016");
+    }
+
+    public boolean checkInput(EditText name, EditText pass) {
+        boolean v0;
+        if((name.getText().toString().isEmpty()) || (pass.getText().toString().isEmpty())) {
+            v0 = false;
+        }
+        else {
+            v0 = true;
+        }
+
+        return v0;
+    }
+
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case 2131492945: {
+                goto label_4;
+            }
+        }
+
+        return;
+    label_4:
+        if(!this.checkInput(this.usrName, this.usrPass)) {
+            goto label_49;
+        }
+
+        if(this.usrName.getText().toString().length() != 16) {
+            goto label_44;
+        }
+
+        Handler v2 = new Handler(this.usrName.getText().toString(), this.usrPass.getText().toString(
+                ));
+        v2.handleInput();
+        new AESEncrpty();
+        try {
+            if(!v2.getPass().equals(AESEncrpty.Encrypt(v2.getName(), v2.getNameCalc()))) {
+                goto label_38;
+            }
+
+            Toast.makeText(this.getApplicationContext(), "Yes", 0).show();
+            return;
+        label_38:
+            Toast.makeText(this.getApplicationContext(), "retry...", 0).show();
+            return;
+        }
+        catch(Exception v1) {
+            v1.printStackTrace();
+            return;
+        }
+
+    label_44:
+        Toast.makeText(this.getApplicationContext(), "retry...", 0).show();
+        return;
+    label_49:
+        Toast.makeText(this.getApplicationContext(), "Plz input sth...", 0).show();
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(2130968601);
+        this.appInit();
+        this.btnCheck.setOnClickListener(((View$OnClickListener)this));
+    }
+}
+        String v4 = null;
+        if(sKey != null && sKey.length() == 16) {
+            SecretKeySpec v3 = new SecretKeySpec(sKey.getBytes("utf-8"), "AES");
+            Cipher v0 = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            v0.init(1, ((Key)v3));
+            v4 = Base64.encodeToString(v0.doFinal(sSrc.getBytes("utf-8")), 0);
+        }
+char[] str = this.str.toCharArray();
+        int i;
+        for(i = 0; i < array.length; ++i) {
+            array[i] = ((char)(array[i] + 3));
+        }
+
+        int v6;
+        for(v6 = 0; true; ++v6) {
+            if(v6 >= array.length) {
+                goto label_31;
+            }
+
+            v8[v6] = str[array[v6] % str.length];
+        }
+
+v8[v6] = str[username[v6] % str.length];
+
+    public encrpty2() {
+        super();
+        this.xorString = "o0xmuhe";
+        this.str = "talkischeapshowmethecode$ischeapshowmethe#SycSYC{IamNotFLAG2333}";
+    }
+for(v0 = 0; v0 < v8.length; ++v0) {
+            v2[v0] = v8[v0] ^ v6[v0 % 7];
+        }
+
+        int v1;
+        for(v1 = 0; v1 < v8.length; ++v1) {
+            v3[v1] = v5[(v2[v1] + 7) % v5.length];
+        }
+'''
+from Crypto.Cipher import AES
+import base64, os
+encrypt1_str = "ThisIsNotFL@GplzDonnotSubmitThisString#HaveFunWithGeekmastergogo"
+username_handle1 = ''
+v8 = ''
+v2 = []
+v8_handle = ''
+username_handle = ''
+xor_string = 'o0xmuhe'
+encrypt2_str = "talkischeapshowmethecode$ischeapshowmethe#SycSYC{IamNotFLAG2333}"
+username = '1' * 16
+
+'''
+padding writing
+'''
+BS = AES.block_size
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s : s[0:-ord(s[-1])]
+
+for j in username:
+    username_handle += chr(ord(j) + 3)
+print "first_handle is: %s"%(username_handle)
+for i in xrange(len(username)):
+    v8 += encrypt1_str[ord(username_handle[i]) % len(encrypt1_str)]
+#print "v8 is: %s"%(v8)
+for k in xrange(len(v8)):
+    v2.append(ord(v8[k]) ^ ord(xor_string[k % 7]))
+#print "v2:"
+#print v2
+for l in xrange(len(v8)):
+    v8_handle += encrypt2_str[(v2[l] + 7) % len(encrypt2_str)]
+#print v8_handle
+key = v8_handle[::-1]
+print "the key is:%s" %(key)
+mode = AES.MODE_ECB
+enc = AES.new(key, mode)
+ciper = base64.b64encode(enc.encrypt(pad(username)))
+print ciper
+
+
+
+
